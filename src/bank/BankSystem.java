57921265fdb690f34490df192978212e32b84df0
Jyothish <jyothishmasarpu@gmail.com>
2026-02-18 09:06:12 +0530

    Tested account creation feature

--- src/bank/BankSystem.java
+++ src/bank/BankSystem.java
@@ -1,5 +1,21 @@
 package bank;
 import java.util.ArrayList;
+import java.util.Scanner;
 public class BankSystem {
-    ArrayList<Account> accounts=new ArrayList<>();
+    ArrayList<Account> accounts = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
+    public void createAccount() {
+        System.out.print("Enter Account Number: ");
+        int accNo = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Enter Name: ");
+        String name = sc.nextLine();
+        System.out.print("Enter PIN: ");
+        int pin = sc.nextInt();
+        System.out.print("Enter Initial Balance: ");
+        double balance = sc.nextDouble();
+        Account acc = new Account(accNo, name, pin, balance);
+        accounts.add(acc);
+        System.out.println("Account Created Successfully");
+    }
 }
--- src/bank/Main.java
+++ src/bank/Main.java
@@ -1,11 +1,25 @@
 package bank;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-
-        Account acc1 = new Account(101, "Jyothish", 1234, 5000);
-
-        System.out.println("Account Created Successfully");
-
+        Scanner sc = new Scanner(System.in);
+        BankSystem bank = new BankSystem();
+        while (true) {
+            System.out.println("\n--- ATM MENU ---");
+            System.out.println("1. Create Account");
+            System.out.println("2. Exit");
+            System.out.print("Choose option: ");
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    bank.createAccount();
+                    break;
+                case 2:
+                    System.out.println("Thank you!");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice");
+            }
+        }
     }
 }
