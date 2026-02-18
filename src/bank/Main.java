d4fa7b8820e388bf137b9cdc8e22833704519077
Jyothish <jyothishmasarpu@gmail.com>
2026-02-18 08:54:18 +0530

    Added Account class and fixed package structure

---
+++ src/bank/Main.java
@@ -0,0 +1,11 @@
+package bank;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Account acc1 = new Account(101, "Jyothish", 1234, 5000);
+
+        System.out.println("Account Created Successfully");
+
+    }
+}
