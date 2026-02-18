d0f0dc7f282eb8a8ad5c94ba42a5f0e5a5ed5bc6
Jyothish <jyothishmasarpu@gmail.com>
2026-02-18 11:30:17 +0530

    Added ATM operations: login, deposit, withdraw, balance

--- src/bank/BankSystem.java
+++ src/bank/BankSystem.java
@@ -1,21 +1,71 @@
 package bank;
+
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class BankSystem {
+
     ArrayList<Account> accounts = new ArrayList<>();
     Scanner sc = new Scanner(System.in);
+
     public void createAccount() {
-        System.out.print("Enter Account Number: ");
+        System.out.print("Enter account number: ");
         int accNo = sc.nextInt();
-        sc.nextLine();
-        System.out.print("Enter Name: ");
-        String name = sc.nextLine();
-        System.out.print("Enter PIN: ");
+
+        System.out.print("Enter name: ");
+        String name = sc.next();
+
+        System.out.print("Enter pin: ");
         int pin = sc.nextInt();
-        System.out.print("Enter Initial Balance: ");
+
+        System.out.print("Enter initial balance: ");
         double balance = sc.nextDouble();
+
         Account acc = new Account(accNo, name, pin, balance);
         accounts.add(acc);
-        System.out.println("Account Created Successfully");
+
+        System.out.println("Account created successfully!");
+    }
+    public Account login() {
+        System.out.print("Enter account number: ");
+        int accNo = sc.nextInt();
+
+        System.out.print("Enter PIN: ");
+        int pin = sc.nextInt();
+
+        for (Account a : accounts) {
+            if (a.accountNumber == accNo && a.pin == pin) {
+                System.out.println("Login successful!");
+                return a;
+            }
+        }
+
+        System.out.println("Invalid details");
+        return null;
+    }
+    public void deposit(Account acc) {
+        System.out.print("Enter amount: ");
+        double amt = sc.nextDouble();
+        acc.balance += amt;
+        System.out.println("Deposit successful");
+    }
+    public void withdraw(Account acc) {
+        System.out.print("Enter amount: ");
+        double amt = sc.nextDouble();
+
+        if (amt <= acc.balance) {
+            acc.balance -= amt;
+            System.out.println("Withdraw successful");
+        } else {
+            System.out.println("Insufficient balance");
+        }
+    }
+    public void checkBalance(Account acc) {
+        System.out.println("Balance: " + acc.balance);
+    }
+    public void showAccounts() {
+        for (Account a : accounts) {
+            System.out.println(a.accountNumber + " " + a.name + " " + a.balance);
+        }
     }
 }
--- src/bank/Main.java
+++ src/bank/Main.java
@@ -1,22 +1,57 @@
 package bank;
+
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         BankSystem bank = new BankSystem();
+
         while (true) {
             System.out.println("\n--- ATM MENU ---");
             System.out.println("1. Create Account");
-            System.out.println("2. Exit");
+            System.out.println("2. Login");
+            System.out.println("3. Exit");
             System.out.print("Choose option: ");
             int choice = sc.nextInt();
+
             switch (choice) {
+
                 case 1:
                     bank.createAccount();
                     break;
+
                 case 2:
+                    Account acc = bank.login();
+
+                    if (acc != null) {
+                        while (true) {
+                            System.out.println("\n1. Deposit");
+                            System.out.println("2. Withdraw");
+                            System.out.println("3. Check Balance");
+                            System.out.println("4. Logout");
+                            System.out.print("Choose: ");
+                            int opt = sc.nextInt();
+
+                            if (opt == 1)
+                                bank.deposit(acc);
+                            else if (opt == 2)
+                                bank.withdraw(acc);
+                            else if (opt == 3)
+                                bank.checkBalance(acc);
+                            else if (opt == 4)
+                                break;
+                            else
+                                System.out.println("Invalid");
+                        }
+                    }
+                    break;
+
+                case 3:
                     System.out.println("Thank you!");
                     System.exit(0);
+
                 default:
                     System.out.println("Invalid choice");
             }
